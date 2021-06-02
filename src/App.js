631b957c7d7b3ce08e148e3679b8d4f9e2cc9b8f
kevin guzman <guzmank685@gmail.com>
2021-06-02 20:32:15 -0300

    se modificaron los archivos import

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { useState, useEffect } from 'react';
-import Admin from './components/administracion/Admin';
+import { useState, useEffect } from "react";
+import Admin from "./components/administracion/Admin";
 import PaginaPrincipal from "./components/paginaPrincipal/PaginaPrincipal";
-import Inicio from './components/common/Inicio';
-import DetalleNoticia from './components/Noticias/DetalleNoticia';
+import Inicio from "./components/common/Inicio";
+import DetalleNoticia from "./components/Noticias/DetalleNoticia";
 
 import Actualidad from "./components/paginaPrincipal/Actualidad";
 import Deportes from "./components/paginaPrincipal/Deportes";
@@ -14,8 +14,8 @@ import Tecnologia from "./components/paginaPrincipal/Tecnologia";
 import Politica from "./components/paginaPrincipal/Politica";
 import Navegacion from "./components/common/Navegacion";
 import Footer from "./components/common/Footer";
-import NuevaNoticia from './components/administracion/NuevaNoticia';
-import EditarNoticia from './components/administracion/EditarNoticia';
+import NuevaNoticia from "./components/administracion/NuevaNoticia";
+import EditarNoticia from "./components/administracion/EditarNoticia";
 
 function App() {
   const URL = process.env.REACT_APP_API_URL;
@@ -47,12 +47,16 @@ function App() {
         <Route exact path="/admin">
           <Admin noticias={noticias} consultarAPI={consultarAPI}></Admin>
         </Route>
- <Route exact path="/admin/nuevanoticia">
-          <NuevaNoticia noticias={noticias} consultarAPI={consultarAPI}> </NuevaNoticia>
+        <Route exact path="/admin/nuevanoticia">
+          <NuevaNoticia noticias={noticias} consultarAPI={consultarAPI}>
+            {" "}
+          </NuevaNoticia>
         </Route>
         <Route exact path="/admin/editarnoticia/:id">
-          <EditarNoticia noticias={noticias} consultarAPI={consultarAPI}></EditarNoticia>
-          
+          <EditarNoticia
+            noticias={noticias}
+            consultarAPI={consultarAPI}
+          ></EditarNoticia>
         </Route>
 
         <Route exact path="/actualidad">
@@ -73,18 +77,12 @@ function App() {
         <Route exact path="/politica">
           <Politica consultarAPI={consultarAPI} noticias={noticias}></Politica>
         </Route>
-        <Route exact path='/noticia'>
+        <Route exact path="/noticia">
           <DetalleNoticia></DetalleNoticia>
         </Route>
         <Route exact path=""></Route>
       </Switch>
       <Footer></Footer>
-
-
-
-
-
-
     </Router>
   );
 }
